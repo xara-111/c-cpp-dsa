c8d4943a23109045d7ca4006a703b8de50265e23
xara-111 <believeinyourself4922@gmail.com>
2024-03-01 06:19:31 +0530

    dsa

---
+++ 1array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+int main()
+{
+    int A[5];
+    A[0]=12;
+    A[1]=15;
+    A[2]=25;
+
+    cout<<sizeof(A)<<endl;
+    cout<<A[1]<<endl;
+    printf("%d\n",A[1]);
+
+    return 0;
+}
