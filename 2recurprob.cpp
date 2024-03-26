5fe7fc427c373403a6a275e6cce3050d39cece14
xara-111 <believeinyourself4922@gmail.com>
2024-03-26 19:46:15 +0530

    dsa

--- 2recurprob.cpp
+++ 2recurprob.cpp
@@ -337,3 +337,21 @@ int main()
     cout<<"Result= "<<c(n,r);
 }
 */
+
+
+#include<stdio.h>
+#include<iostream>
+void TOH(int n, int A, int B, int C)
+{
+    if(n>0)
+    {
+        TOH(n-1, A,C,B);
+        printf("(from %d to %d)\n",A,C);
+        TOH(n-1,B,A,C);
+    }
+}
+int main()
+{
+    TOH(5,1,2,3);
+    return 0;
+}
