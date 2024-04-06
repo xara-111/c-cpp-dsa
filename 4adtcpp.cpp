c48c497a279d84df1983d25953500d4618f90f43
xara-111 <believeinyourself4922@gmail.com>
2024-04-06 21:59:12 +0530

    dsa

--- 4adtcpp.cpp
+++ 4adtcpp.cpp
@@ -1,35 +1,46 @@
+//WRITING A CLASS IN C++
+//USING TEMPLATE FOR VARIOUS DATATYPES
+
+
 #include<iostream>
 using namespace std;
+
+//This is an Array of type integer.
+//If we want a float type array then it's possible to make a generic class in c++
+template<class T>    //Generic array
+
 class Array //class should have data members and members functions
 {
 private:
-     int *A; //Not a fixed size array but a pointer so that i can dynamically create an array
-     int size;
+     T *A; //Not a fixed size array but a pointer so that i can dynamically create an array.
+    //Inside the class wherever i want generic datatype i should write T there.
+     int size; 
      int length;
 public:
 //C++ class must have constructor and destructor 
      Array()  //non-parameterized constructor
      {
         size=10; //set the default size
-        A=new int[10]; //create an array of default size
+        A=new T[10]; //create an array of default size
         length=0; //initially no element
      }
      Array(int sz) //parameterized constructor to create an array of desired size
      {
         size=sz;
         length=0;
-        A=new int[size];
+        A=new T[size];
      }
      ~Array()
      {
         delete []A;
      }
      void Display();
-     void Insert(int index, int x);
-     int Delete(int index);
+     void Insert(int index, T x);
+     T Delete(int index);
 };
 
-void Array::Display()
+template <class T> //For each function, we should write it and use it.
+void Array<T>::Display() //Write T here since class is template type
 {
     for(int i=0;i<length;i++)
     {
@@ -38,7 +49,8 @@ void Array::Display()
     cout<<endl;
 }
 
-void Array::Insert(int index, int x)
+template <class T>
+void Array<T>::Insert(int index, T x)
 {
     if(index>=0 && index<=length)
     {
@@ -51,9 +63,10 @@ void Array::Insert(int index, int x)
     }
 }
 
-int Array::Delete(int index)
+template <class T>
+T Array<T>::Delete(int index)
 {
-    int x=0;
+    T x=0;
     if(index>=0 && index<length)
     {
         x=A[index];
@@ -68,10 +81,13 @@ int Array::Delete(int index)
 
 int main()
 {
-    Array arr(10); //Create array object of same size
+    //Create array object of same size
+    Array<float> arr(10); //In bracket write any datatype to use(now, float)
     arr.Insert(0,5);
-    arr.Insert(1,6);
+    arr.Insert(1,6.4);
     arr.Insert(2,9);
+    arr.Display();
+    cout<<arr.Delete(0)<<endl;
     arr.Display();
 
     return 0;
