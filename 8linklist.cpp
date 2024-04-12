71ff235bb5eb9fa4a4c8270153e7a899a7cc1c02
xara-111 <believeinyourself4922@gmail.com>
2024-04-13 01:57:33 +0530

    dsa

--- 8linklist.cpp
+++ 8linklist.cpp
@@ -196,39 +196,168 @@ void Insert(struct Node *p, int index, int x)
     }
 }
 
-int main()
-{
-    int A[]={3, 5, 7, 10, 15}; //create link list using these elements
-    create(A,5);
-    Display(first);
-    Rdisplay(first);
-    Rrevdisplay(first);
-    printf("Length is %d\n ",count(first));
-    printf("Length is %d\n ",Rcount(first));
-    printf("Sum is %d\n",add(first));
-    printf("Sum is %d\n",Radd(first));
-    printf("Maximum is %d\n",max(first));
-    printf("Maximum is %d\n",Rmax(first));
-    printf("Maximum is %d\n",RTmax(first));
 
-    printf("Key is at %p\n",search(first, 10));
-    
-    struct Node *temp;
-    temp=Search(first,7);  //Replace different search funtions
-    temp=Search(first,10); //Could be searched faster the 2nd time at constant time 
-    if(temp)
+void Sortedinsert(struct Node *p, int x)
+{
+    struct Node *t, *q=NULL;
+    t=(struct Node *)malloc(sizeof(struct Node));
+    t->data=x;
+    t->next=NULL;
+
+//Is the first node created yet? If it's the first node, pointer first would be null
+//Initially if there's no node at all, t should be the first node
+    if(first==NULL)  //If the first node doesnt exist, make first point on t.
+        first=t;
+    else
     {
-        printf("Key is found %d\n",temp->data);
+        while(p && p->data<x)
+        {
+            q=p;
+            p=p->next;
+        }
+        //The place where we've stopped is it the first node?If it's first node, new node will be inserted on left hand side
+        //If the element we're inserting is smallest then it'll come before the first node.
+        //So if p is staying upon first node only we've to insert new node before first node
+        if(p==first)
+        {
+            t->next=first;
+            first=t;
+        }
+        else
+        {
+            t->next=q->next;
+            q->next=t;
+        }
+    }
+}
+
+int Delete(struct Node *p, int index)
+{
+    Node *q;
+    int x=-1;
+    int i;
+
+    if(index < 1 ||index > count(p))
+          return -1;
+    if(index==1)
+    {
+        p=first;
+        x=first->data;
+        first=first->next;
+        free(p);
+        return x;
     }
     else
     {
-        printf("Key not found\n");
+        p=first;
+        q=NULL;
+        for(i=0; i<index-1 && p; i++)
+        {
+            q=p;
+            p=p->next;
+        }
+        if(p)
+        {
+            q->next=p->next;
+            x=p->data;
+            free(p);
+        }
     }
+    return x;
+}
 
-    Display(first); //Notice the key move to front as first node
+int isSorted(struct Node *p)
+{
+    int x=INT32_MIN;
+    while(p!=NULL)
+    {
+        if(p->data <x)
+            return false;
+        x=p->data;
+        p=p->next;
+    }
+    return true;
+}
 
-    Insert(first,3,2);  //We can call insert function multiple time for creating the whole link list
-    Display(first);
+
+void Duplicate(struct Node *p)
+{
     
+    Node *q=first->next;
+    while(q!=NULL)
+    {
+        if(p->data!=q->data)
+        {
+            p=q;
+            q=q->next;
+        }
+        else
+        {
+            p->next=q->next;
+            free(q);
+            q=p->next;
+        }
+    }
+}
+
+
+int main()
+{
+    int A[]={3, 5, 7,10,15}; //create link list using these elements
+    create(A,5);
+    // Display(first);
+    // Rdisplay(first);
+    // Rrevdisplay(first);
+    // printf("Length is %d\n ",count(first));
+    // printf("Length is %d\n ",Rcount(first));
+    // printf("Sum is %d\n",add(first));
+    // printf("Sum is %d\n",Radd(first));
+    // printf("Maximum is %d\n",max(first));
+    // printf("Maximum is %d\n",Rmax(first));
+    // printf("Maximum is %d\n",RTmax(first));
+
+    // printf("Key is at %p\n",search(first, 10));
+    
+    // struct Node *temp;
+    // temp=Search(first,7);  //Replace different search funtions
+    // temp=Search(first,10); //Could be searched faster the 2nd time at constant time 
+    // if(temp)
+    // {
+    //     printf("Key is found %d\n",temp->data);
+    // }
+    // else
+    // {
+    //     printf("Key not found\n");
+    // }
+
+    // Display(first); //Notice the key move to front as first node
+
+
+
+    // Insert(first,3,2);  //We can call insert function multiple time for creating the whole link list
+    // Display(first);
+
+    // Sortedinsert(first,6);
+    // printf("\n\n");
+    // Display(first);
+
+    // printf("Deleted Element is %d\n",Delete(first,5));
+    // Display(first);
+
+    // if(isSorted(first))
+    // {
+    //     printf("Sorted\n");
+    // }
+    // else
+    // {
+    //     printf("Not sorted\n");
+    // }
+
+    // Duplicate(first);
+    // Display(first);
+
+
+    Reverse(first);
+    Display(first);
 
 }
