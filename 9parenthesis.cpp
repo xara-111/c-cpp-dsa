0551b8866f2398afd04915e96662fc4815f450fd
xara-111 <believeinyourself4922@gmail.com>
2024-04-16 23:30:24 +0530

    dsa

---
+++ 9parenthesis.cpp
@@ -0,0 +1,68 @@
+//NOT RUNNING THO :(
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    char data;
+    struct Node *next;
+}*top=NULL; 
+
+void push(char x)
+{
+    struct Node *t;
+    t=(struct Node *)malloc(sizeof(struct Node));
+    if(t==NULL)
+         printf("Stack is full\n");
+    else
+    {
+        t->data=x;
+        t->next=top;
+        top=t;
+    }
+}
+
+char pop()
+{
+    struct Node *t;
+    char x=-1;
+    if(top==NULL)
+        printf("Stack is Empty\n");
+    else
+    {
+        t=top;
+        top=top->next;
+        x=t->data;
+        free(t);
+    }
+    return x;
+}
+
+
+
+int isBalanced(char *exp)
+{
+    int i;
+    for(i=0; exp[i]!='\0'; i++)
+    {
+        if(exp[i]=='(')
+            push(exp[i]);
+        else if(exp[i]==')')
+        {
+            if(top==NULL)
+                return 0;
+            pop();
+        }
+    }
+    if(top==NULL)
+        return 1;
+    else
+        return 0;
+}
+int main()
+{
+   char *exp= (char *)"((a+b)*(c-d))";
+
+   printf("%d ",isBalanced(exp));
+}
--- 9postfix.cpp
+++ 9postfix.cpp
@@ -1,15 +1,17 @@
+//Evaluation of postfix
+
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include<strings.h>
 
 struct Node
 {
-    char data;
+    int data;
     struct Node *next;
 }*top=NULL; 
 
-void push(char x)
+void push(int x)
 {
     struct Node *t;
     t=(struct Node *)malloc(sizeof(struct Node));
@@ -23,10 +25,10 @@ void push(char x)
     }
 }
 
-char pop()
+int pop()
 {
     struct Node *t;
-    char x=-1;
+    int x=-1;
     if(top==NULL)
         printf("Stack is Empty\n");
     else
@@ -81,8 +83,9 @@ int isOperand(char x)
 char * IntoPost(char *infix)
 {
     int i=0,j=0;
-    char *postfix;
     int len= strlen(infix);
+
+    char *postfix;
     postfix=(char *)malloc((len+2)*sizeof(char));
     while(infix[i]!='\0')
     {
@@ -103,13 +106,39 @@ char * IntoPost(char *infix)
     postfix[j]='\0';
     return postfix;
 }
+
+int Eval(char *postfix)
+{
+    int i=0;
+    int x1,x2,r;
+    for(i=0; postfix[i]!='\0'; i++)
+    {
+        if(isOperand(postfix[i]))
+        {
+            push(postfix[i]-'0');
+        }
+        else
+        {
+            x2=pop();
+            x1=pop();
+
+            switch(postfix[i])
+            {
+                case '+':r=x1+x2; break;
+                case '-':r=x1-x2; break;
+                case '*':r=x1*x2; break;
+                case '/':r=x1/x2; break;
+            }
+            push(r);
+        }
+    }
+    return top->data;
+}
+
 int main()
 {
-    push('#');
-  char *infix=(char *)"a+b*c-d/e";
-  char *postfix=IntoPost(infix);
-
-  printf("%s ",postfix);
+    char *postfix=(char *)"234*+82/-";
+    printf("Result is %d\n",Eval(postfix));
 }
 
 
