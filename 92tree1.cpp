30b3d85b799d04b29c5370afd1e41f0a69b8b2a7
xara-111 <believeinyourself4922@gmail.com>
2024-04-21 18:07:18 +0530

    dsa

--- 92tree1.cpp
+++ 92tree1.cpp
@@ -1,3 +1,5 @@
+//TREE
+
 #include<stdio.h>
 #include<stdlib.h>
 
---
+++ 93bst.cpp
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node //Tree node
+{
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+}*root=NULL;
+
+void Insert(int key)
+{
+    struct Node *t=root;
+    struct Node *r,*p;
+
+    if(root==NULL)
+    {
+        p=(struct Node *)malloc(sizeof(struct Node));
+        p->data=key;
+        p->lchild=p->rchild=NULL;
+        root=p;
+        return;
+    }
+    while(t!=NULL)
+    {
+        r=t;
+        if(key<t->data)
+          t=t->lchild;
+        else if(key>t->data)
+             t=t->rchild;
+        else
+            return;
+    }
+    p=(struct Node *)malloc(sizeof(struct Node));
+    p->data=key;
+    p->lchild=p->rchild=NULL;
+
+    if(key<r->data)
+        r->lchild=p;
+    else
+        r->rchild=p;
+}
+
+void Inorder(struct Node *p)
+{
+    if(p)
+    {
+        Inorder(p->lchild);
+        printf("%d ",p->data);
+        Inorder(p->rchild);
+    }
+}
+
+struct Node * Search(int key)
+{
+    struct Node *t=root;
+    while(t!=NULL)
+    {
+        if(key==t->data)
+           return t;
+        else if(key<t->data)
+           t=t->lchild;
+        else
+            t=t->rchild;
+    }
+    return NULL;
+}
+
+struct Node *Rinsert(struct Node *p,int key)
+{
+    if(p==NULL)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=key;
+
+    }
+    if(key<p->data)
+       p->lchild=Rinsert(p->lchild,key);
+    else if(key>p->data)
+       p->rchild=Rinsert(p->rchild,key);
+
+    return p;
+}
+int main()
+{
+    struct Node *temp;
+
+    Insert(10);
+    Insert(5);
+    Insert(20);
+    Insert(8);
+    Insert(30);
+    Inorder(root);
+    printf("\n");
+
+    temp=Search(20);
+    if(temp!=NULL)
+        printf("Element %d is found\n",temp->data);
+    else
+        printf("Element is not found\n");
+}
